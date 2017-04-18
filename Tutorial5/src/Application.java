a10f765f9464a226439a27f82a889805c647b9e0
Sami Peralahti <sami.peralahti@gmail.com>
2017-04-18 15:28:20 +0300

    created a for loop

---
+++ Tutorial5/src/Application.java
@@ -0,0 +1,12 @@
+public class Application {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 5; i++) {
+			// %d gets replaced with the provided interger
+			System.out.printf("The value of i is: %d\n ", i);
+			System.out.println("My name is Kappa");
+		}
+	}
+
+}
