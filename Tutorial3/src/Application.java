d61cc180ff485cd91e56e3d64f47468d92f818ca
Sami Peralahti <sami.peralahti@gmail.com>
2017-04-18 15:00:08 +0300

    learned to use Strings

---
+++ Tutorial3/src/Application.java
@@ -0,0 +1,21 @@
+
+public class Application {
+	public static void main(String[] args) {
+		int myInt = 7;
+		
+		String text = "Hello";
+
+		String blank = " ";
+		
+		String name = "Bob";
+		
+		String greeting = text + blank + name;
+		
+		System.out.println(greeting);
+		
+		System.out.println("Hello" + " " + "Bob");
+		
+		System.out.println("My interger is: " + myInt);
+	
+	}
+}
